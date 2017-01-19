0a8bee5873654b2085f267ce38af42f436757e07
wuthmone <whlaing10@gmail.com>
2017-01-19 16:19:34 +0800

    Added files

--- server.R
+++ server.R
@@ -11,16 +11,26 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
-  
+        
+        output$plot1 <- renderPlot({
+                set.seed(2017-01-19)
+                number_of_points <- input$numeric
+                minX <- input$sliderX[1]
+                maxX <- input$sliderX[2]
+                minY <- input$sliderY[1]
+                maxY <- input$sliderY[2]
+                
+                dataX <- runif(number_of_points,minX,maxX)
+                dataY <- runif(number_of_points,minY,maxY)
+                
+                xlab <- ifelse(input$show_xlab,"X Axsis","")
+                ylab <- ifelse(input$show_ylab,"Y Axsis","")
+                
+                main <- ifelse(input$show_title,"Title","")
+                
+                plot(dataX,dataY, xlab=xlab,ylab=ylab,main = main,
+                     xlim = c(-100,100), ylim = c(-100,100))
+                
+        })
+        
 })
--- ui.R
+++ ui.R
@@ -11,23 +11,33 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
+        
+        # Application title
+        titlePanel("Plot Random Numbers"),
+        
+        # Sidebar with a slider input for number of bins 
+        sidebarLayout(
+                sidebarPanel(
+                        numericInput("numeric","How many Random Numnber should be plotted?",
+                                     value = 1000, min = 1, max = 1000, step = 1),
+                        sliderInput("sliderX", "Pick Min and Max X values",
+                                    -100,100, value = c(-50,50)),
+                        
+                        sliderInput("sliderY", "Pick Min and Max X values",
+                                    -100,100, value = c(-50,50)),
+                        checkboxInput("show_xlab","Show/Hide X Axis Label",
+                                      value = T),
+                        checkboxInput("show_ylab","Show/Hide Y Axis Label",
+                                      value = T),   
+                        checkboxInput("show_title", "Show/Hide Title")
+                        
+                ),
+                
+                # Show a plot of the generated distribution
+                mainPanel(
+                        h3("Graph of Random Points"),
+                        plotOutput("plot1")
+                        
+                )
+        )
 ))
