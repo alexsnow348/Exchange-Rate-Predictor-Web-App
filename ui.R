11aa616affc0698310e3fdec99f7c29fa443a5bb
wuthmone <whlaing10@gmail.com>
2017-01-19 16:52:31 +0800

    Added files

--- ui.R
+++ ui.R
@@ -18,25 +18,21 @@ shinyUI(fluidPage(
         # Sidebar with a slider input for number of bins 
         sidebarLayout(
                 sidebarPanel(
-                        numericInput("numeric","How many Random Numnber should be plotted?",
-                                     value = 1000, min = 1, max = 1000, step = 1),
-                        sliderInput("sliderX", "Pick Min and Max X values",
-                                    -100,100, value = c(-50,50)),
+                        textInput("firstDay", "First Day", value = "", width = NULL, placeholder = "Enter First Day Value"),
+                        textInput("secondDay", "Second Day", value = "", width = NULL, placeholder = "Enter First Day Value"),
+                        textInput("thirdDay", "Third Day", value = "", width = NULL, placeholder = "Enter First Day Value")
                         
-                        sliderInput("sliderY", "Pick Min and Max X values",
-                                    -100,100, value = c(-50,50)),
-                        checkboxInput("show_xlab","Show/Hide X Axis Label",
-                                      value = T),
-                        checkboxInput("show_ylab","Show/Hide Y Axis Label",
-                                      value = T),   
-                        checkboxInput("show_title", "Show/Hide Title")
                         
                 ),
                 
                 # Show a plot of the generated distribution
                 mainPanel(
-                        h3("Graph of Random Points"),
-                        plotOutput("plot1")
+                        h3("Error"),
+                        #textOutput("error", container = if (inline) span else div, inline = FALSE),
+                        h3("Acutal"),
+                       # textOutput("actual", container = if (inline) span else div, inline = FALSE),
+                        h3("Predicted Value")
+                       # textOutput("predicted", container = if (inline) span else div, inline = FALSE)
                         
                 )
         )
